32e3924cdc90592301807b526ba19de54e1a1597
KiPTM <mathewtuwei@gmail.com>
2023-07-25 04:38:48 -0700

    advance

--- get_flags.c
+++ get_flags.c
@@ -2,11 +2,14 @@
 
 /**
  * get_flags - Calculates active flags
+ *
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
- * Return: Flags:
+ * Return: Flags
+ *
  */
 int get_flags(const char *format, int *i)
+
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
