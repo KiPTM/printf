7140bdb191c7f9343fbb86d67793873476646657
Joyline <133635575+Joytwin@users.noreply.github.com>
2023-07-26 15:14:33 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,34 +1,38 @@
 #include "main.h"
-
 /**
- *get_flags - Calculates active flags
- *@format: Formatted string in which to print the arguments
- *@i: take a parameter.
- *Return: Flags:
+ * get_flags - Calculate active flags in a formatted string.
+ *
+ * This function scans the provided formatted string to find and calculate
+ * the active flags that represent various formatting options.
+ *
+ * @format: The formatted string to search for flags.
+ * @index: Pointer to the current index in the format string.
+ *
+ * Return: An integer representing the active flags (bitmask).
  */
-int get_flags(const char *format, int *i)
+int get_flags(const char *format, int *index)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
-	int j, curr_i;
+	int j, current_index;
 	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	const char FLAGS_CHARS[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_VALUES[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (current_index = *index + 1; format[current_index] != '\0'; current_index++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+		for (j = 0; FLAGS_CHARS[j] != '\0'; j++)
+		{
+			if (format[current_index] == FLAGS_CHARS[j])
 			{
-				flags |= FLAGS_ARR[j];
+				flags |= FLAGS_VALUES[j];
 				break;
 			}
+		}
 
-		if (FLAGS_CH[j] == 0)
+		if (FLAGS_CHARS[j] == 0)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*index = current_index - 1;
 
 	return (flags);
 }
