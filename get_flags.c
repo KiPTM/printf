51709336bba65746ffea658777e6de59139b3ac7
Joyline <133635575+Joytwin@users.noreply.github.com>
2023-07-26 15:07:02 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * get_flags - calculates active flags
- * @formart: formatted string in which to print the arguements 
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
- * Return: flags
+ * Return: Flags:
  */
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
-	/* 1 2 4 8 16 */
+	/* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
-	const char FLAGS_CH[] = {'_', '+', '0', '#', ' ', '\0'}; 
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
@@ -24,8 +24,8 @@ int get_flags(const char *format, int *i)
 				break;
 			}
 
-	if (FLAGS_CH[I] == 0)
-		break;
+		if (FLAGS_CH[j] == 0)
+			break;
 	}
 
 	*i = curr_i - 1;
